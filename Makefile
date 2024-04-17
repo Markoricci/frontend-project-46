c510265d912ec7c7058db08bb84a07438e063784
Mark <markusgor855@yandex.ru>
2024-04-17 14:25:23 +0300

    3

--- Makefile
+++ Makefile
@@ -1,19 +1,15 @@
 install: 
 	npm ci
 
-run:
-	bin/gendiff.js
-
-test:
-	npm test
-
-test-coverage:
-	npm test -- --coverage --coverageProvider=v8
+gendiff:
+	node bin/gendiff.js
 
 lint:
 	npx eslint .
 
 lint-fix:
-	npx eslint . --fix
+	npx eslint --fix .
 
-.PHONY: test
+test:
+	npm test
+	
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>', 'required file path to get diff')
+  .argument('<filepath2>', 'required file path to get diff')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
+program.parse();
